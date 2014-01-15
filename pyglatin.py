667dc315dc753928acfb397fb246771b728a8b19
Evalle <shmarnev@gmail.com>
2014-01-15 22:25:51 +0500

    Update pyglatin.py
    Release version :)

--- pyglatin.py
+++ pyglatin.py
@@ -1,6 +1,20 @@
-print "Welcome to my own the English to Pig Latin translator! v 0.01"
-original = raw_input("What's you name?")
-if len(original) >= 0 and original.isalpha():
-    print original
-else: 
-    print "empty"
+print "Welcome to my own the English to Pig Latin translator! v 1.0"
+
+# Here some variables
+
+pyg = 'ay'
+original = raw_input('Please, enter your word:')
+word = original.lower()
+first = word[0]
+new_vowel = word + pyg # if first letter is vowel
+new_consonant = word[1:] + first + pyg # if first letter is consonant
+
+if len(original) > 0 and original.isalpha():
+    if first == "a" or first == "e" or first == "i" or first ==  "o" or first == "u": 
+        print "Ok, your word is VOWEL" 
+        print "And your Pyglatin word is: " + new_vowel
+    else:
+        print "Ok, your word is starting with CONSONANT"
+        print "And your Pyglatin word is: " + new_consonant
+else:
+    print 'empty'
