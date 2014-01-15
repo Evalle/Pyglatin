b9c08e237c2ee964781d3fe23e245610808640af
Evalle <shmarnev@gmail.com>
2014-01-15 21:19:09 +0500

    Create pyglatin.py

---
+++ pyglatin.py
@@ -0,0 +1,6 @@
+print "Welcome to my own the English to Pig Latin translator! v 0.01"
+original = raw_input("What's you name?")
+if len(original) >= 0 and original.isalpha():
+    print original
+else: 
+    print "empty"
